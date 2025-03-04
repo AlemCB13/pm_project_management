a5933ea1b08bf9a339b39f19e8e0e37e573e437c
Cristian Alemán <bladimiralemanc@gmail.com>
2025-03-04 02:20:44 -0500

    refactor: :white_check_mark: fixed some trouble files on docker and docker-compose

--- app/endpoints/projects.py
+++ app/endpoints/projects.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models, schemas, database
+import httpx
 
 router = APIRouter()
+API_GATEWAY_URL = "http://pm_api_gateway:8000"
 
 # Dependencia para obtener la sesión de la base de datos
 def get_db():
@@ -12,13 +14,28 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/projects/", response_model=schemas.Project)
-def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
-    db_project = models.Project(**project.dict())
-    db.add(db_project)
-    db.commit()
-    db.refresh(db_project)
-    return db_project
+@router.post("/projects/")
+async def create_project(project: schemas.ProjectCreate):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{API_GATEWAY_URL}/api/users/{project.owner_id}")
+        if response.raise_for_status != 200:
+            raise HTTPException(status_code=404, detail="User not found")
+        # Crear el proyecto
+        project_data = {
+            "name": project.name,
+            "description": project.description,
+            "owner_id": project.owner_id,
+        }
+        response = await client.post(f"{API_GATEWAY_URL}/api/projects", json=project_data)
+        return response.json()
+
+# @router.post("/projects/", response_model=schemas.Project)
+# def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
+#     db_project = models.Project(**project.dict())
+#     db.add(db_project)
+#     db.commit()
+#     db.refresh(db_project)
+#     return db_project
 
 @router.get("/projects/{project_id}", response_model=schemas.Project)
 def read_project(project_id: int, db: Session = Depends(get_db)):
